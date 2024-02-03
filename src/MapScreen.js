150468ccb9d921ba5888f3b9d4acb7bf1f7956a8
Ahsan <ahsansatti402@gmail.com>
2024-02-03 20:28:29 +0500

    implement-single-marker

--- src/MapScreen.js
+++ src/MapScreen.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import MapView from 'react-native-maps';
-
+import MapView, {Marker} from 'react-native-maps';
 const MapScreen = () => {
   return (
     <View>
       <MapView
         style={{width: '100%', height: '100%'}}
         initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
+          latitude: 33.62571806303041,
+          longitude: 73.11862946994432,
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1,
+        }}>
+        <Marker
+          coordinate={{
+            latitude: 33.62571806303041,
+            longitude: 73.11862946994432,
+          }}
+          title={'I am here'}
+          description={'this is my home destination'}
+        />
+      </MapView>
     </View>
   );
 };
